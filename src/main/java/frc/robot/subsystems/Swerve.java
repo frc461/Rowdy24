2f6cb5cc03040a34ea45e7c03e487451430329e0
TidyMoMan <77814251+TidyMoMan@users.noreply.github.com>
2024-01-16 15:16:54 -0500

    tidying up swerve

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -183,6 +183,19 @@ public class Swerve extends SubsystemBase {
         }
     }
 
+    // Puts the wheels in an X configuration
+    public void setXMode(){
+        SwerveModuleState Xstates[] = {new SwerveModuleState(), new SwerveModuleState(), new SwerveModuleState(), new SwerveModuleState()};
+
+        for(int i = 0; i < 5; i++){
+            Xstates[i].speedMetersPerSecond = 0.0;
+            Xstates[i].angle.equals(new Rotation2d().fromDegrees(i*90)); //Rotates each successive wheel 90 degrees further
+        }
+
+        setModuleStates(Xstates);
+    }
+
+    //attempts to rotate the drivetrain to a given value
     public void rotateToDegree(double target){
         PIDController rotController = new PIDController(0.1, 0.0008, 0.001);
         rotController.enableContinuousInput(-180, 180);
@@ -192,19 +205,14 @@ public class Swerve extends SubsystemBase {
         drive(new Translation2d(0, 0), -.25*rotate, false, true);        
     }
 
-    // public void setXMode(){
-        // Constants.Mod0.setAngle(new SwerveModuleState(0.0, Rotation2d.fromDegrees(45)));
-        // .setAngle(new SwerveModuleState(0.0, Rotation2d.fromDegrees(45+90)));
-        // SwerveModule.setAngle(new SwerveModuleState(0.0, Rotation2d.fromDegrees(45+90+90)));
-        // SwerveModule.setAngle(new SwerveModuleState(0.0, Rotation2d.fromDegrees(45+90+90+90)));
-    // }
-
+    //Old and possibly deprecated (depreciated) function
     public void limelightDrive(Limelight limelight){
         while (limelight.getRZ() > 0.65) {
             drive(new Translation2d(.1,0),0,false,false);
         }
     }
 
+    //2023 autobalance function
     public void autoBalance(){
         double target = 0;
         System.out.println("Autobalance Start");
