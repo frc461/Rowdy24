cda4fd9511b26ed0407425072d331d13ca265f95
TidyMoMan <77814251+TidyMoMan@users.noreply.github.com>
2024-01-23 21:59:10 -0500

    added new subsystem files

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase{
+    private CANSparkMax intake;
+    //DigitalInput cubeBeam = new DigitalInput(0);
+    //DigitalInput coneBeam = new DigitalInput(1);
+    
+    private DigitalOutput intakeIndicator = new DigitalOutput(4);
+
+    public boolean intakeCube = false;
+
+    public Intake() {
+        intake = new CANSparkMax(33, MotorType.kBrushed);
+        intake.restoreFactoryDefaults();
+        intake.setInverted(true);
+        led.setLength(ledData.getLength());
+        //lights.showLights("blue");
+        
+    }
+
+
+}
