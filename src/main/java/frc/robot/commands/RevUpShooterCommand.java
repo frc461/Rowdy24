bd347645c6054a557f14602644b58b42a4e5852c
ez500 <eugenezhang500@gmail.com>
2024-02-17 11:26:25 -0500

    rev shooter command

---
+++ src/main/java/frc/robot/commands/RevUpShooterCommand.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Shooter;
+
+import java.awt.*;
+
+public class RevUpShooterCommand extends Command {
+    private final Shooter shooter;
+    private final Limelight limelight;
+    private final boolean idleMode;
+    public RevUpShooterCommand(Shooter shooter, Limelight limelight, boolean idleMode) {
+        this.shooter = shooter;
+        this.limelight = limelight;
+        this.idleMode = idleMode;
+        addRequirements(this.shooter);
+    }
+
+    @Override
+    public void initialize() {
+        shooter.shoot(Constants.Shooter.BASE_SHOOTER_SPEED + limelight.getRZ() * Constants.Shooter.DISTANCE_MULTIPLIER);
+    }
+
+    @Override
+    public void execute() {
+        shooter.shoot(Constants.Shooter.BASE_SHOOTER_SPEED + limelight.getRZ() * Constants.Shooter.DISTANCE_MULTIPLIER);
+    }
+
+    @Override
+    public void end(boolean isFinished) {
+        shooter.setShooterIdle(idleMode);
+    }
+}
