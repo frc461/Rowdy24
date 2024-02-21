f8d4ee9ea40aef8edeb1586fc9f547106ef81cee
TidyMoMan <77814251+tidymoman@users.noreply.github.com>
2024-02-21 18:24:16 -0500

    merge pls...?

--- src/main/java/frc/robot/subsystems/IntakeCarriage.java
+++ src/main/java/frc/robot/subsystems/IntakeCarriage.java
@@ -39,7 +39,7 @@ public class IntakeCarriage extends SubsystemBase {
     }
 
     public boolean getAmpBeamBroken() {
-        return ampBeam.get();
+        return !ampBeam.get();
     }
 
     public boolean getShooterBeamBroken() {
