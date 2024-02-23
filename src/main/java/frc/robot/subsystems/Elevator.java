68c96ed474c7b9cbc70aa34944d18a26f842ecf7
daniel <daniel@andymark.com>
2024-02-23 16:28:17 -0500

    Added feedforward controller to elevator

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -9,7 +9,8 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.controller.ElevatorFeedforward;
-import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -17,7 +18,7 @@ import frc.robot.Constants;
 
 public class Elevator extends SubsystemBase {
     private final TalonFX elevator;
-    private final PIDController pidController, upperPidController, lowerPidController;
+    private final ProfiledPIDController pidController, upperPidController, lowerPidController;
     private final DigitalInput elevatorSwitch = new DigitalInput(Constants.Elevator.ELEVATOR_LIMIT_SWITCH);
     private final Servo elevatorClamp = new Servo(Constants.Elevator.ELEVATOR_SERVO_PORT);
     private final ElevatorFeedforward elevatorFeedforward = new ElevatorFeedforward(
@@ -35,25 +36,30 @@ public class Elevator extends SubsystemBase {
         config.Voltage = new VoltageConfigs().withPeakForwardVoltage(6);
         config.MotorOutput = new MotorOutputConfigs().withInverted(InvertedValue.Clockwise_Positive).withNeutralMode(NeutralModeValue.Brake);
         config.CurrentLimits = new CurrentLimitsConfigs().withSupplyCurrentLimit(Constants.Elevator.ELEVATOR_CURRENT_LIMIT);
-        
+        TrapezoidProfile.Constraints trapConstraints = new TrapezoidProfile.Constraints(0.5,0.2);
+
         elevator.getConfigurator().apply(config);
         
-        pidController = new PIDController(
+        pidController = new ProfiledPIDController(
                 Constants.Elevator.ELEVATOR_P,
                 Constants.Elevator.ELEVATOR_I,
-                Constants.Elevator.ELEVATOR_D
+                Constants.Elevator.ELEVATOR_D,
+                trapConstraints
         );
         
-        lowerPidController = new PIDController(
+        lowerPidController = new ProfiledPIDController(
                 Constants.Elevator.ELEVATOR_P,
                 Constants.Elevator.ELEVATOR_I,
-                Constants.Elevator.ELEVATOR_D
+                Constants.Elevator.ELEVATOR_D,
+                trapConstraints
         );
 
-        upperPidController = new PIDController(
+        
+        upperPidController = new ProfiledPIDController(
                 Constants.Elevator.ELEVATOR_P,
                 Constants.Elevator.ELEVATOR_I,
-                Constants.Elevator.ELEVATOR_D
+                Constants.Elevator.ELEVATOR_D,
+                trapConstraints
         );
 
         elevatorClamp.set(Constants.Elevator.ELEVATOR_SERVO_UNCLAMPED_POS);
@@ -88,11 +94,16 @@ public class Elevator extends SubsystemBase {
         checkLimitSwitch();
         if(elevator.getRotorPosition().getValueAsDouble() > Constants.Elevator.UPPER_STAGE_THRESHOLD){ //if on upper stage use higher PID
             if (limitHitOnce) {
-            elevator.set(upperPidController.calculate(elevator.getRotorPosition().getValueAsDouble(), target));
+                double pid = upperPidController.calculate(elevator.getRotorPosition().getValueAsDouble(), target);
+                double ff = elevatorFeedforward.calculate(upperPidController.getSetpoint().velocity);
+            elevator.set(ff + pid);
+            
             }
         }else{ //otherwise use lower PID
             if (limitHitOnce) {
-            elevator.set(lowerPidController.calculate(elevator.getRotorPosition().getValueAsDouble(), target));
+                double pid = lowerPidController.calculate(elevator.getRotorPosition().getValueAsDouble(), target);
+                double ff = elevatorFeedforward.calculate(lowerPidController.getSetpoint().velocity);
+            elevator.set(ff + pid);
             }   
         }
     }
