b5995a9987b91a7d3bf74e5e03bd4c0a9d448430
ez500 <eugenezhang500@gmail.com>
2024-02-23 00:14:31 -0500

    changed initialization time

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -31,13 +31,13 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotInit() {
+        RobotIdentity.initializeConstants();
         ctreConfigs = new CTREConfigs();
         /*
          * Instantiate our RobotContainer. This will perform all our button bindings,
          * and put our autonomous chooser on the dashboard.
          */
         robotContainer = new RobotContainer();
-        RobotIdentity.initializeConstants();
     }
 
     /**
