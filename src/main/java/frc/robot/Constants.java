44fabdb5a642da599124521a14cd8d6123383f7e
ez500 <eugenezhang500@gmail.com>
2024-03-29 22:42:48 -0400

    increased shuttle speed a tiny bit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -133,7 +133,8 @@ public final class Constants {
         public static final double IDLE_SHOOTER_POWER = 0.2;
 
         // shuttle power out of full power 1
-        public static final double SHUTTLE_SHOOTER_POWER = 0.4167;
+        // TODO: test shuttle power
+        public static final double SHUTTLE_SHOOTER_POWER = 0.46;
 
         // baseline shooter speed in RPM
         public static final double BASE_SHOOTER_SPEED = 6000;
