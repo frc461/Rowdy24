5a9af3bca74ae82709cb45022dde83b8e0b461fb
ez500 <eugenezhang500@gmail.com>
2024-04-04 20:39:38 -0400

    update limelight megatag2

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -112,9 +112,9 @@ public final class Constants {
 
     public static final class Limelight {
         // pid for limelight alignment
-        public static final double LIMELIGHT_P = 0.01;
-        public static final double LIMELIGHT_I = 0.0001;
-        public static final double LIMELIGHT_D = 0.0008;
+        public static final double LIMELIGHT_P = 0.04;
+        public static final double LIMELIGHT_I = 0;
+        public static final double LIMELIGHT_D = 0;
 
         public static final double CAMERA_FORWARD = 0.0223774;
         public static final double CAMERA_UP = 0.5916676;
