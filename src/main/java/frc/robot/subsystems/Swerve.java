744182dda8ff444930c214fed3706c9260450b0c
ez500 <eugenezhang500@gmail.com>
2024-04-10 22:08:03 -0400

    amend auto builder

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -65,10 +65,7 @@ public class Swerve extends SubsystemBase {
 
         AutoBuilder.configureHolonomic(
                 this::getFusedPoseEstimator, // Robot pose supplier
-                newPose -> {
-                    this.setFusedPoseEstimator(newPose);
-                    this.setOdometry(newPose);
-                }, // Method to reset odometry (will be called if your auto has a starting pose)
+                this::setFusedPoseEstimator, // Method to reset odometry (will be called if your auto has a starting pose)
                 () -> Constants.Swerve.SWERVE_KINEMATICS.toChassisSpeeds(getModuleStates()), // ChassisSpeeds supplier.
                                                                                              // MUST BE ROBOT RELATIVE
                 speeds -> {
